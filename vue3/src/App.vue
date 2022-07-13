0c285ff8aabfe0d5135d5d5a9de223acc184604a
yanghao <yanghao18328@163.com>
2022-07-13 23:45:52 +0800

    feat:init

---
+++ vue3/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  {{ count }}
+  {{ double }}
+  <button @click="increase">+1</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { reactive, toRefs, computed } from "vue";
+
+interface dataProps {
+  count: number;
+  double: number;
+  increase: () => number;
+}
+export default defineComponent({
+  name: "App",
+  setup() {
+    const data: dataProps = reactive({
+      count: 0,
+      double: computed(() => data.count * 2),
+      increase: () => data.count++,
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
